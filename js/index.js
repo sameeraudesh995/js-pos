361095eb6fa20298a91da340c8388734fe313542
sameeraudesh995 <sameeraudesh.2225@gmail.com>
2025-02-07 08:51:58 +0530

    login and register function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,53 @@
-const clearContainer=()=>{
+class User{
+    constructor(fullName, email, password) {
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+    }
+}
+
+let userArray=[];
+
+// =================user management================== //
+
+const createUser=()=>{
+
+    const fullName=document.getElementById('fullName').value;
+    const email=document.getElementById('registerEmail').value;
+    const password=document.getElementById('registerPassword').value;
+
+   // const hash= bcrypt.hashSync(password,bcrypt.genSaltSync(10));
+   // console.log(hash);
+
+
+    let createdUser=new User(fullName, email, password);
+    let existsUserData=userArray.find(e=>e.email==email);
+    if(existsUserData){
+        alert('user email already exists');
+        return;//programme terminate
+    }
+    userArray.push(createdUser);
+    console.log(userArray)
+}
+const login=()=>{
+    const email=$('#email').val();
+    const password=$('#password').val();
+
+    let existsUserData=userArray.find(e=>e.email==email);
+
+    if(existsUserData){
+        if(existsUserData.password==password){
+            alert('login success');
+        }else {
+            alert('password incorrect');
+            return;
+        }
+    }else{
+        alert('user not found');
+    }
+}
+const clearAndLoad=(element)=>{
 
 }
+
+const createCustomer=()=>{}
